1b7f25251e1c82ae23244c7dee9b4dd9d75e5e10
wildKahoona <kahoona@gmx.ch>
2017-11-24 00:34:33 +0100

    Letzte Fehler geflickt

--- onlineshop-war/WebContent/WEB-INF/db_script/onlineshop.sql
+++ onlineshop-war/WebContent/WEB-INF/db_script/onlineshop.sql
@@ -1,11 +1,17 @@
 DROP TABLE item;
-DROP TABLE rating;
 DROP TABLE customer;
 DROP TABLE category;
 DROP TABLE condition;
 
 CREATE TABLE customer(
 id			NUMBER(19) PRIMARY KEY,
+role   		VARCHAR2(45) NOT NULL,
+firstname	VARCHAR2(40) NOT NULL,
+lastname	VARCHAR2(40) NOT NULL,
+street		VARCHAR2(100) NOT NULL,
+zip			INTEGER NOT NULL,
+city		VARCHAR2(40) NOT NULL,
+country		VARCHAR2(40) NOT NULL,
 email		VARCHAR2(40) NOT NULL UNIQUE,
 password	VARCHAR2(10) NOT NULL
 			CHECK(LENGTH(password)>=6)
@@ -44,6 +50,10 @@ condition_id NUMBER(19) NOT NULL,
 seller_id	NUMBER(19) NOT NULL,
 buyer_id	NUMBER(19),
 sold		TIMESTAMP(3),
+seller_ratingstars  INTEGER,
+seller_ratingcomment VARCHAR2(1000),
+buyer_ratingstars  INTEGER,
+buyer_ratingcomment VARCHAR2(1000),
 CONSTRAINT fk_category
 	FOREIGN KEY (category_id) REFERENCES category (id),
 CONSTRAINT fk_condition
@@ -55,21 +65,6 @@ CONSTRAINT fk_buyer
 );
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON item TO onlineshop_user;
-
-CREATE TABLE rating (
-id			NUMBER(19) PRIMARY KEY,
-from_id		NUMBER(19) NOT NULL,
-to_id		NUMBER(19) NOT NULL,
-stars		INTEGER NOT NULL,
-commentary	VARCHAR2(1000) NOT NULL,
-CONSTRAINT fk_from
-	FOREIGN KEY (from_id) REFERENCES customer (id),
-CONSTRAINT fk_to
-	FOREIGN KEY (to_id) REFERENCES customer (id)
-);
-GRANT SELECT, INSERT, UPDATE, DELETE
-ON rating TO onlineshop_user;
-
 
 DROP SEQUENCE seq_customer;
 CREATE SEQUENCE seq_customer;
@@ -86,10 +81,6 @@ GRANT ALL ON seq_condition TO onlineshop_user;
 DROP SEQUENCE seq_item;
 CREATE SEQUENCE seq_item;
 GRANT ALL ON seq_item TO onlineshop_user;
-
-DROP SEQUENCE seq_rating;
-CREATE SEQUENCE seq_rating;
-GRANT ALL ON seq_rating TO onlineshop_user;
 
 CREATE OR REPLACE TRIGGER tri_customer
 BEFORE INSERT ON customer
@@ -119,24 +110,19 @@ BEGIN :NEW.id := seq_item.NEXTVAL;
 END;
 /
 
-CREATE OR REPLACE TRIGGER tri_rating
-BEFORE INSERT ON rating
-FOR EACH ROW
-BEGIN :NEW.id := seq_rating.NEXTVAL;
-END;
-/
-
 COMMIT;
 
 INSERT into onlineshop.category (description) values ('Auto');
-INSERT into onlineshop.category (description) values ('Bücher');
+INSERT into onlineshop.category (description) values ('Computer');
+INSERT into onlineshop.category (description) values ('Haushalt');
 INSERT into onlineshop.category (description) values ('Kleider');
 INSERT into onlineshop.category (description) values ('Musik');
-INSERT into onlineshop.category (description) values ('Möbel');
+INSERT into onlineshop.category (description) values ('Sport');
 
 INSERT into onlineshop.condition (description) values ('Neu');
 INSERT into onlineshop.condition (description) values ('Gebraucht');
 INSERT into onlineshop.condition (description) values ('Defekt');
 
-COMMIT;
+INSERT into onlineshop.CUSTOMER (role, firstname, lastname, street, zip, city, country, email, password) values ('ROLE_ADMIN', 'Admin', 'Admin', 'Adminstrasse 1', 8080, 'Zürich', 'CH', 'admin@gmx.ch', '123456');
 
+COMMIT;
